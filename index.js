b03178f5b1a12c077f3d2a195c082c00020d237d
Vo Thanh Quoc <tquoc1233@gmail.com>
2019-09-17 16:59:19 +0700

    use lowdb and create id random

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 var express = require('express');
+var low = require('lowdb');
+var shortid = require('shortid');
+
+
 var app = express();
 var port = 3000;
 
+//lowdb
+
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+var db =  low(adapter);
+
+
+
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+db.defaults({ users: [] })
+  .write();
 
 // pug
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-var users = [
-			{id:1, name: 'Quoc'},
-			{id:2, name: 'Doan'},
-			{id:3, name: 'Khanh'},
-		];
+var users = db.get('users').value();
 
 app.get('/', function(req, res) {
 	res.render('index', {
@@ -22,6 +33,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/users', function(req, res) {
+	console.log(users);
 	res.render('users/index', {
 		users: users,
 		name: ''
@@ -46,11 +58,24 @@ app.get('/users/create', function(req, res) {
 });
 
 app.post('/users/create', function(req, res) {
-	console.log(req.body);
+	console.log('body',req.body);
+	console.log('user_before',users);
+
 	// res.json(req.body);
-	var newUser = {id: users.length+1, name: req.body.name};
-	users.push(newUser);
+	// create id random
+	req.body.id = shortid.generate();
+
+	db.get('users').push(req.body).write();
+	console.log('user_after',users);
 	res.redirect('/users');
+});
+
+app.get('/users/:id', function(req, res) {
+	var id = req.params.id;
+
+	res.render('users/detail', {
+		user: db.get('users').find({id:id}).value()
+	});
 });
 
 
